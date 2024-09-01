b8d26741c729afda7ade0d78dd9a54b7aad31b6a
rashikghosh <rashikghosh2004@gmail.com>
2024-09-01 11:42:15 +0530

    Data structures and algorithms file setup

---
+++ Data Structures and algorithms/Arrays.cpp
@@ -0,0 +1,51 @@
+//
+//  Arrays.cpp
+//  Data Structures and algorithms
+//
+//  Created by Rashik Ghosh on 01/09/24.
+//
+
+//-------------Reverse the array----------
+//#include <iostream>
+//using namespace std;
+//int main(){
+//    int n;
+//    cout << "enter the value of n: ";
+//    cin >> n;
+//    int array[n];
+//    for (int i=0; i<n; i++) {
+//        cin >> array[i];
+//    }
+//    for (int i=n-1; i>=0; i--) {
+//        cout << array[i] << " ";
+//    }
+//}
+//---------------find the maximun and minimun element in an array------
+
+//#include<iostream>
+//using namespace std;
+//int main(){
+//    int n;
+//    cout << "enter the value of n: ";
+//    cin >> n;
+//    int array[n];
+//    for (int i=0; i<n; i++) {
+//        cin >> array[i];
+//    }
+//    int min=array[0];
+//    int max=array[0];
+//    for (int i=0; i<n; i++) {
+//        if (array[i]<min) {
+//            min=array[i];
+//        }
+//        else if (array[i]>max){
+//            max=array[i];
+//        }
+//    }
+//    cout <<"The minimum number in an array is: " << min << endl;
+//    cout << "The maximum number in an array is: " << max << endl;
+//    return 0;
+//}
+
+//--------------Find the "Kth" max and min element of an array-----------
+
