186830685adf1c7d691e45110d1bd8cb092c830f
rashikghosh <rashikghosh2004@gmail.com>
2024-09-02 19:27:58 +0530

    Find the "Kth" max and min element of an array

--- Data Structures and algorithms/Arrays.cpp
+++ Data Structures and algorithms/Arrays.cpp
@@ -48,4 +48,34 @@
 //}
 
 //--------------Find the "Kth" max and min element of an array-----------
-
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    int array[n];
+    for(int i=0;i<n;i++){
+        cin >> array[i];
+    }
+    for (int i=0; i<n-1; i++) {
+        for (int j=0; j<n-i-1; j++) {
+            if (array[j]>array[j+1]) {
+                int temp=array[j];
+                array[j]=array[j+1];
+                array[j+1]=temp;
+            }
+        }
+    }
+    for (int i=0; i<n; i++) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+    int k;
+    cout << "Enter which element you want to choose: ";
+    cin >> k;
+    int min=array[k-1];
+    int max=array[n-k];
+    cout << "The min element of an array is: "<< min <<endl;
+    cout << "The max elemnet of an array is: "<< max <<endl;
+}
