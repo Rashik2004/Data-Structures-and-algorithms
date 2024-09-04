5bc9e02615da01ad8832f8ddaa4f28589f449d61
rashikghosh <rashikghosh2004@gmail.com>
2024-09-04 10:07:21 +0530

    commit 4

--- Data Structures and algorithms/Leetcode/Leetcode Problems.cpp
+++ Data Structures and algorithms/Leetcode/Leetcode Problems.cpp
@@ -1,8 +1,31 @@
 //
 //  Leetcode Problems.cpp
-//  Data Structures and algorithms
-//
 //  Created by Rashik Ghosh on 04/09/24.
-//
+//||___________________________________________________________EASY___________________________________________________________
+//Give an integer number n; return the difference between the product of its digit and the sum of its digit...
 
-#include "Leetcode Problems.hpp"
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter the number num: ";
+    cin >> num;
+
+    int num1;
+    int sum = 0;
+    int mult = 1;
+
+    while (num != 0) {
+        num1 = num % 10;
+        sum += num1;
+        mult *= num1;
+        num /= 10;
+    }
+
+    cout << "Sum of the digits of the number is: " << sum << endl;
+    cout << "Product of the digits of the number is: " << mult << endl;
+    cout << "Difference between the product and sum of the number is: " << mult - sum << endl;
+
+    return 0;
+}
