66e821d2689f68dbf57d866af6fd25481ccc7154
rashikghosh <rashikghosh2004@gmail.com>
2024-09-09 12:11:23 +0530

    linkedlist

---
+++ Data Structures and algorithms/Linked list.cpp
@@ -0,0 +1,8 @@
+//
+//  Linked list.cpp
+//  Data Structures and algorithms
+//
+//  Created by Rashik Ghosh on 09/09/24.
+//
+
+#include "Linked list.hpp"
---
+++ Data Structures and algorithms/Linked list.hpp
@@ -0,0 +1,13 @@
+//
+//  Linked list.hpp
+//  Data Structures and algorithms
+//
+//  Created by Rashik Ghosh on 09/09/24.
+//
+
+#ifndef Linked_list_hpp
+#define Linked_list_hpp
+
+#include <stdio.h>
+
+#endif /* Linked_list_hpp */
