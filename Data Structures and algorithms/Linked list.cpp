045bd317b0b664b10b73335107b150b3ef1bcb77
rashikghosh <rashikghosh2004@gmail.com>
2024-10-10 02:08:01 +0530

    10.10.10

--- Data Structures and algorithms/Linked list.cpp
+++ Data Structures and algorithms/Linked list.cpp
@@ -30,108 +30,205 @@
 
 //basic operations in linked list
 
-#include "Linked list.hpp"
+//#include "Linked list.hpp"
+//
+//struct node {
+//int data;
+//node* next;
+//};
+//
+//// Function to append node at the end of the list
+//void appendNode(node*& head, int data) {
+//node* newNode = new node();
+//newNode->data = data;
+//newNode->next = nullptr;
+//if (head == nullptr) {
+//head = newNode;
+//} else {
+//node* temp = head;
+//while (temp->next != nullptr) {
+//temp = temp->next;
+//}
+//temp->next = newNode;
+//}
+//}
+//
+//// Function to append node at the beginning of the list
+//void appendNodeAtBeginning(node*& head, int data) {
+//node* newNode = new node();
+//newNode->data = data;
+//newNode->next = head; // Corrected this line
+//head = newNode;
+//}
+//
+//// Function to append node at a specific position
+//void appendNodeAtChoice(node*& head, int data, int pos) {
+//if (pos <= 0) {
+//cout << "Invalid position!" << endl;
+//return;
+//}
+//
+//node* newNode = new node();
+//newNode->data = data;
+//
+//if (pos == 1) {
+//newNode->next = head;
+//head = newNode;
+//return;
+//}
+//
+//node* temp = head;
+//for (int i = 1; i < pos - 1 && temp != nullptr; i++) {
+//temp = temp->next;
+//}
+//
+//if (temp == nullptr) {
+//cout << "Position out of range!" << endl;
+//delete newNode; // Free the memory as the position is invalid
+//return;
+//}
+//
+//newNode->next = temp->next;
+//temp->next = newNode;
+//}
+//
+//// Function to print the linked list
+//void printlist(node* head) {
+//node* temp = head;
+//cout << "Updated list: ";
+//while (temp != nullptr) {
+//cout << temp->data << " ";
+//temp = temp->next;
+//}
+//cout << endl;
+//}
+//
+//int main() {
+//int n;
+//cout << "Enter how many elements you want to enter: ";
+//cin >> n;
+//
+//node* head = nullptr;
+//int data;
+//
+//for (int i = 0; i < n; i++) {
+//cout << "Enter the data element: ";
+//cin >> data;
+//appendNode(head, data);
+//}
+//
+//printlist(head);
+//
+//cout << "Enter the element to insert at the beginning: ";
+//int key;
+//cin >> key;
+//appendNodeAtBeginning(head, key);
+//printlist(head);
+//
+//int k, pos;
+//cout << "Enter the position in which you want to insert: ";
+//cin >> pos;
+//cout << "Enter the element which you want to insert: ";
+//cin >> k;
+//appendNodeAtChoice(head, k, pos);
+//printlist(head);
+//
+//return 0;
+//}
 
-struct node {
-int data;
-node* next;
-};
+// Implementation of grounded header linked list
 
-// Function to append node at the end of the list
-void appendNode(node*& head, int data) {
-node* newNode = new node();
-newNode->data = data;
-newNode->next = nullptr;
-if (head == nullptr) {
-head = newNode;
-} else {
-node* temp = head;
-while (temp->next != nullptr) {
-temp = temp->next;
-}
-temp->next = newNode;
-}
-}
-
-// Function to append node at the beginning of the list
-void appendNodeAtBeginning(node*& head, int data) {
-node* newNode = new node();
-newNode->data = data;
-newNode->next = head; // Corrected this line
-head = newNode;
-}
-
-// Function to append node at a specific position
-void appendNodeAtChoice(node*& head, int data, int pos) {
-if (pos <= 0) {
-cout << "Invalid position!" << endl;
-return;
-}
-
-node* newNode = new node();
-newNode->data = data;
-
-if (pos == 1) {
-newNode->next = head;
-head = newNode;
-return;
-}
-
-node* temp = head;
-for (int i = 1; i < pos - 1 && temp != nullptr; i++) {
-temp = temp->next;
-}
-
-if (temp == nullptr) {
-cout << "Position out of range!" << endl;
-delete newNode; // Free the memory as the position is invalid
-return;
-}
-
-newNode->next = temp->next;
-temp->next = newNode;
-}
-
-// Function to print the linked list
-void printlist(node* head) {
-node* temp = head;
-cout << "Updated list: ";
-while (temp != nullptr) {
-cout << temp->data << " ";
-temp = temp->next;
-}
-cout << endl;
-}
-
-int main() {
-int n;
-cout << "Enter how many elements you want to enter: ";
-cin >> n;
-
-node* head = nullptr;
-int data;
-
-for (int i = 0; i < n; i++) {
-cout << "Enter the data element: ";
-cin >> data;
-appendNode(head, data);
-}
-
-printlist(head);
-
-cout << "Enter the element to insert at the beginning: ";
-int key;
-cin >> key;
-appendNodeAtBeginning(head, key);
-printlist(head);
-
-int k, pos;
-cout << "Enter the position in which you want to insert: ";
-cin >> pos;
-cout << "Enter the element which you want to insert: ";
-cin >> k;
-appendNodeAtChoice(head, k, pos);
-printlist(head);
-
-return 0;
-}
+//#include<iostream>
+//using namespace std;
+//
+//struct node {
+//    int data;
+//    node*next;
+//};
+//
+//node *initaialiseHeader(){
+//    node*header= new node();
+//    header->data=0;
+//    header->next=nullptr;
+//    return header;
+//}
+//
+//void appendNode(node*header,int data){
+//    node*newnode=new node();
+//    newnode->data=data;
+//    newnode->next=nullptr;
+//    node*temp=header;
+//    while (temp->next!=nullptr) {
+//        temp=temp->next;
+//    }
+//    temp->next=newnode;
+//    header->data++;
+//}
+//
+//void appendNodeAtBeginning(node*header,int data){
+//    node*newnode=new node();
+//    newnode->data=data;
+//    newnode->next=header->next;
+//    header->next=newnode;
+//    header->data++;
+//}
+//
+//void appendNodeAtChoice(node*header,int pos,int data){
+//    node*newnode=new node();
+//    newnode->data=data;
+//    newnode->next=nullptr;
+//    node *temp=header->next;
+//    for (int i=1; i<pos-1 && temp->next!=nullptr; i++) {
+//        temp=temp->next;
+//    }
+//    
+//    if (temp->next==nullptr) {
+//        cout<<"Invalid postion";
+//        return;
+//    }
+//    if (pos==1) {
+//        newnode->next=header->next;
+//        header->next=newnode;
+//        header->data++;
+//    }
+//    newnode->next=temp->next;
+//    temp->next=newnode;
+//    header->data++;
+//}
+//
+//void printList(node*header){
+//    node*temp=header;
+//    while (temp!=nullptr) {
+//        cout<<temp->data<<" ";
+//        temp=temp->next;
+//    }
+//}
+//
+//int main(){
+//    node*header=initaialiseHeader();
+//    int n;
+//    cout << "enter the numbr if nodes you want to insert: ";
+//    cin >> n;
+//    int data;
+//    for (int i=0; i<n; i++) {
+//        cin>>data;
+//        appendNode(header, data);
+//    }
+//    printList(header);
+//    cout << endl;
+//    int value;
+//    cout << "enter the value you want to insert: ";
+//    cin>>value;
+//    appendNodeAtBeginning(header, value);
+//    cout<<endl;
+//    int postion,key;
+//    cout << "enter the postion you want to insert: ";
+//    cin>>postion;
+//    cout << "enter the key: ";
+//    cin>>key;
+//    appendNodeAtChoice(header, postion, key);
+//    printList(header);
+//    cout<<endl;
+//    return 0;
+//}
+//
--- Data Structures and algorithms/Stack.cpp
+++ Data Structures and algorithms/Stack.cpp
@@ -5,4 +5,199 @@
 //  Created by Rashik Ghosh on 23/09/24.
 //
 
-#include <stdio.h>
+//#include <iostream>
+//using namespace std;
+//
+//void push(int stack[],int max,int &top,int x){
+//    if(top>max-1){
+//        cout << "Overflow";
+//    }
+//    else{
+//        top++;
+//        stack[top]=x;
+//        cout << x <<"Pushed inside stack";
+//    }
+//}
+//
+//void pop(int stack[],int &top){
+//    if (top<0) {
+//        cout << "underflow";
+//    }
+//    else{
+//        cout << stack[top]<<"poped from the stack";
+//        top--;
+//    }
+//}
+//
+//void peek(int stack[],int &top){
+//    if (top<0) {
+//        cout<<"no element";
+//    }
+//    else{
+//        cout << stack[top];
+//    }
+//}
+//
+//void isempty(int stack[],int &top){
+//    if (top<0) {
+//        cout << "no elements";
+//    }
+//    else{
+//        cout << "present";
+//    }
+//}
+//
+//void display(int stack[],int &top){
+//    if (top<0) {
+//        cout << "no elements";
+//    }
+//    else{
+//        for (int i=top; i>=0; i--) {
+//            cout << stack[i]<<" ";
+//        }
+//    }
+//}
+//
+//int main(){
+//    int choice = 0;
+//    int top=-1;
+//    int max;
+//    cout << "enter the size: ";
+//    cin >> max;
+//    int stack[max];
+//    int data;
+//    do{
+//        cout <<"what do you want to choose: " << endl;
+//        cout <<"1. insert an element " << endl;
+//        cout <<"2. delete: " << endl;
+//        cout <<"3. peek: " << endl;
+//        cout <<"4. isempty: " << endl;
+//        cout <<"5. display: " << endl;
+//        cout <<"6. exit: " << endl;
+//        cout <<"enter your choice" <<endl;
+//        cin >> choice;
+//        
+//        switch(choice){
+//            case 1:
+//                cout << "enter data";
+//                cin >> data;
+//                push(stack, max, top, data);
+//                break;
+//            case 2:
+//                pop(stack, top);
+//                break;
+//            case 3:
+//                peek(stack, top);
+//                break;
+//            case 4:
+//                isempty(stack, top);
+//                break;
+//            case 5:
+//                display(stack, top);
+//                break;
+//            case 6:
+//                cout << "exiting the stack and displaying the data:";
+//                display(stack, top);
+//                cout << endl;
+//                break;
+//            default:
+//                cout << "invalid";
+//                display(stack, top);
+//                cout << endl;
+//                break;
+//        }
+//    }while(choice==1||choice==2||choice==3||choice==4||choice==5);
+//}
+
+#include "Linked list.hpp"
+struct node{
+    int data;
+    node*next;
+};
+node*top=nullptr;
+void push(int x){
+    node *newNode = new node();
+            if (newNode == nullptr) {
+                cout << "Overflow" << endl;
+                return;
+            }
+        newNode->data = x;
+        newNode->next = top;
+        top = newNode;
+        cout << x << " pushed inside stack" << endl;
+}
+
+void pop(){
+    if (top == nullptr) {
+            cout << "Underflow" << endl;
+            return;
+        }
+        node *temp = top;
+        cout << top->data << " is popped\n";
+        top = top->next;
+        delete temp;
+}
+
+void peek(){
+    if (top == nullptr) {
+            cout << "Underflow" << endl;
+            return;
+        }
+        cout << top->data << " ";
+}
+
+bool isEmpty() {
+    return top == nullptr;
+}
+
+void displayStack(){
+    if (top ==nullptr) {
+        cout <<"underflow";
+        return;
+    }
+    else{
+        node*temp=top;
+        while (temp!=nullptr) {
+            cout << temp->data;
+            temp=temp->next;
+        }
+    }
+}
+int main(){
+    int choice, value;
+    do{
+        cout <<"what do you want to choose: " << endl;
+        cout <<"1. insert an element " << endl;
+        cout <<"2. delete: " << endl;
+        cout <<"3. peek: " << endl;
+        cout <<"4. isempty: " << endl;
+        cout <<"5. display: " << endl;
+        cout <<"6. exit: " << endl;
+        cout <<"enter your choice" <<endl;
+        cin >> choice;
+        switch (choice) {
+            case 1:
+                cout << "Enter value to push";
+                cin >> value;
+                push(value);
+                break;
+            case 2:
+                pop();
+                break;
+            case 3:
+                peek();
+                break;
+            case 4:
+                isEmpty();
+                break;
+            case 5:
+                displayStack();
+                break;
+            case 6:
+                cout << "You chose to exit\n";
+                break;
+            default:
+                cout << "Invalid choice\n";
+        }
+    }while(choice != 6);
+}
